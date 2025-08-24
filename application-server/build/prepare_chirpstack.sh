6416cbf105487bdc24623b9870990ddfded16dbf
Szymon Acedański <accek@mimuw.edu.pl>
2025-08-24 18:53:21 +0200

    application-server: bump to 4.x

--- application-server/build/prepare_chirpstack.sh
+++ application-server/build/prepare_chirpstack.sh
@@ -8,5 +8,5 @@ apt-get install -y crudini
 
 # Prepare Chirpstack packages
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1CE2AFD36DBCCA00
-echo "deb https://artifacts.chirpstack.io/packages/3.x/deb stable main" | tee /etc/apt/sources.list.d/chirpstack.list
+echo "deb https://artifacts.chirpstack.io/packages/4.x/deb stable main" | tee /etc/apt/sources.list.d/chirpstack.list
 apt-get update
